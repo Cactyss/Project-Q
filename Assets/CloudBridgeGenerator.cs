3dd9a2536ccc2741521bd423ea74a8d396d379f7
Cactyss <139386859+Cactyss@users.noreply.github.com>
2023-10-27 12:42:25 -0400

    more level, platform generator done, next is bone?

---
+++ Assets/CloudBridgeGenerator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class CloudBridgeGenerator : MonoBehaviour
+{
+    public GameObject parent;
+    public GameObject platform;
+    public float waitTime;
+    public float offset;
+    public Vector2 NewVelocity;
+    public Vector3 NewTransform;
+    float realWaitTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        realWaitTime = waitTime - offset;
+        StartCoroutine(BridgeGen());
+
+    }
+    private IEnumerator BridgeGen()
+    {
+        yield return new WaitForSeconds(offset);
+        GameObject newPlatform = Instantiate(platform, NewTransform, new Quaternion(0,0,0,0));
+        newPlatform.transform.parent = parent.transform;
+        newPlatform.GetComponent<Rigidbody2D>().velocity = NewVelocity;
+        yield return new WaitForSeconds(realWaitTime);
+        Debug.Log(realWaitTime + offset);
+        StartCoroutine(BridgeGen());
+
+    }
+}
+ 
---
+++ Assets/DestroyAfterAwile.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class DestroyAfterAwile : MonoBehaviour
+{
+    float waitTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = 10f;
+    }
+    private IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        Destroy(gameObject);
+    }
+}
