c95ddb51036147e33332e224a784392df48c326d
Cactyss <139386859+Cactyss@users.noreply.github.com>
2024-02-14 15:10:05 -0500

    level 2 started, added player movement

---
+++ Assets/Scripts/Generic/NextScene.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    public GameObject timerObject;
+    public int delay;
+    private void Start()
+    {
+        if (timerObject != null)
+        { 
+            timerObject.GetComponent<SpeedrunTimer>().SetSpeedrunTime();
+        }
+        StartCoroutine(GoNextScene());
+    }
+    private IEnumerator GoNextScene()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            yield return new WaitForSeconds(delay);
+            Debug.Log("load next level");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            yield return new WaitForSeconds(delay);
+            Debug.Log("load next level");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
---
+++ Assets/Scripts/Generic/SpeedrunTimer.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpeedrunTimer : MonoBehaviour
+{
+    public String currentTime;
+    public String bestTime1;
+    public String bestTime2;
+    public GameObject text;
+    DateTime startTime;
+    public TimeSpan timeElapsed { get; private set; }
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            startTime = DateTime.Now;
+            PlayerPrefs.SetString("StartTime", startTime.ToString());
+        }
+        else 
+        { 
+            startTime = System.DateTime.Parse(PlayerPrefs.GetString("startTime")); 
+        }
+    }
+    private void Update()
+    {
+        this.timeElapsed = DateTime.Now - startTime;
+        currentTime = timeElapsed.ToString();
+        //output to text
+        text.GetComponent<TextMeshProUGUI>().SetText(timeElapsed.ToString());
+    }
+    public void SetSpeedrunTime()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            bestTime1 = currentTime;
+            PlayerPrefs.SetString("bestTime1", bestTime1);
+        }
+        else if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            bestTime2 = currentTime;
+            PlayerPrefs.SetString("bestTime2", bestTime2);
+        }
+    }
+}
