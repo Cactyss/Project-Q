60750368fd28c5748571bb66a04c70347d7f3ce1
Cactyss <139386859+Cactyss@users.noreply.github.com>
2024-03-23 19:32:35 -0400

    now using //TODO:, added more dialogue code
    //todo is epic and i love it, i hope i remember that im using it come tommorrow! (i wont!)

---
+++ Assets/MyAssets/Final Files/Dialogue/Burito.ink
@@ -0,0 +1,21 @@
+AAAAAAA<b>AAA</b>AAAA<color=red><size=50>AAAAA</color>A</size>AAA #speaker:brandon #portrait:brandon_neutral #layout:left 
+-> main
+
+
+=== main ===
+Do you play league?#speaker:brandon #portrait:brandon_neutral #layout:left
+ + yasuo [] sucks#speaker:brandon #portrait:brandon_neutral #layout:left
+ 
+ + adc [] sucks#speaker:brandon #portrait:brandon_neutral #layout:left
+ 
+ + yep [] that sucks#speaker:brandon #portrait:brandon_neutral #layout:left
+
+- Don't trust it!#speaker:mystery #portrait:mystery_neutral #layout:right
+
+again?#speaker:brandon #portrait:brandon_neutral #layout:left
++ [yes]
+-> main
+
++[no]
+leave, this is a really long line that should ideally take up two lines worth of text on the unity textmeshproUGUI text input system when it's printed letter by letter i mean character by character, if it doesn't look nice i can find a fix on the internet or maybe there is one in a future video on the playlist guide that i am watching!#speaker:brandon #portrait:brandon_neutral #layout:left
+-> END
--- Assets/Scripts/Level 2/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Level 2/Dialogue/DialogueManager.cs
@@ -9,21 +9,30 @@ using UnityEngine.SearchService;
 using System;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine.Rendering;
+using System.Data;
 
 public class DialogueManager : MonoBehaviour
 {
     private const string SPEAKER_TAG = "speaker";
     private const string PORTRAIT_TAG = "portrait";
     private const string LAYOUT_TAG = "layout";
+    //TODO: add ink tags (or tie to the portrait tag): sound effect, font, font color, font size, delay until next dialogue
+    [Header("Text Parameters")]
+    [SerializeField] private float typingSpeed = 0.04f;
 
-    private KeyCode SubmitKeybind;
+    private Coroutine displayLineCoroutine;
+    private bool canContinueToNextLine = false;
+    private bool canSkipTyping = true;
+    // TODO: implement keybinds (this is the continue dialogue keybind)
+    private KeyCode SubmitKeybind = KeyCode.Space;
     private static DialogueManager instance;
-
+    private AnimationClip[] portraitClips;
     [Header("Dialogue UI")]
     [SerializeField] private GameObject dialoguePanel;
     [SerializeField] private TextMeshProUGUI dialogueText;
     [SerializeField] private TextMeshProUGUI displayNameText;
     [SerializeField] private Animator portraitAnimator;
+    [SerializeField] private GameObject canContinueIcon;
     private Animator layoutAnimator;
 
     [Header("Choices UI")]
@@ -45,10 +54,10 @@ public class DialogueManager : MonoBehaviour
     private void Start()
     {
         layoutAnimator = dialoguePanel.GetComponent<Animator>();
-        SubmitKeybind = KeyCode.Space;
         dialogueIsPlaying = false;
         dialoguePanel.SetActive(false);
         choicesText = new TextMeshProUGUI[choices.Length];
+        portraitClips = portraitAnimator.runtimeAnimatorController.animationClips;
         int index = 0;
         foreach (GameObject choice in choices)
         {
@@ -63,7 +72,7 @@ public class DialogueManager : MonoBehaviour
             return;
         }
 
-        if (Input.GetKeyDown(SubmitKeybind))
+        if (Input.GetKeyDown(SubmitKeybind) && currentStory.currentChoices.Count == 0 && canContinueToNextLine)
         {
             ContinueStory();
         }
@@ -76,26 +85,32 @@ public class DialogueManager : MonoBehaviour
 
     public void EnterDialogueMode(TextAsset inkJSON)
     {
-        //reset dialouge  UI
-        displayNameText.text = "???";
-        portraitAnimator.Play("default");
-        layoutAnimator.Play("right");
-
         currentStory = new Story(inkJSON.text);
         dialogueIsPlaying = true;
         dialoguePanel.SetActive(true);
 
+        resetTags();
         ContinueStory();
+    }
+    private void resetTags()
+    {
+        //reset dialouge  UI
+        displayNameText.text = "???";
+        //portraitAnimator.Play("default");
+        layoutAnimator.Play("right");
     }
 
     private void ContinueStory()
     {
         if (currentStory.canContinue)
-        {
+        {   
+            //if a line is already being displayed, stop it
+            if (displayLineCoroutine != null)
+            {
+                StopCoroutine(displayLineCoroutine);
+            }
             //do we make it go character by character right here?
-            dialogueText.text = currentStory.Continue();
-            // display choices if any
-            DisplayChoices();
+            displayLineCoroutine = StartCoroutine(DisplayLine(currentStory.Continue()));
 
             HandleTags(currentStory.currentTags);
         }
@@ -113,6 +128,55 @@ public class DialogueManager : MonoBehaviour
         dialogueText.text = "";
     }
 
+    private IEnumerator DisplayLine(string line)
+    {
+        int letters = 0;
+        //hides certian UI while typing out dialogue
+        canContinueIcon.SetActive(false);
+        HideChoices();
+        
+        //dont allow player to skip onto the next line before they see the current line fully printed out
+        canContinueToNextLine = false;
+
+        bool isAddingRichTextTag = false;
+
+        //empty the displayed text
+        dialogueText.text = "";
+        foreach (char letter in line.ToCharArray())
+        {
+            Debug.Log(letters);
+            //allows the dialogue to be skipped after 3 characters are displayed
+            if (/*canSkipTyping && */Input.GetKey(SubmitKeybind) && letters > 5)
+            {
+                dialogueText.text = line;
+                break;
+            }
+
+            if (letter == '<' || isAddingRichTextTag)
+            {//add the rich text tag info and stuff
+               
+                isAddingRichTextTag = true;
+                dialogueText.text += letter;
+                if (letter == '>')
+                {
+                    isAddingRichTextTag = false;
+                }
+            }
+            else
+            {//add the letter and play a sound
+                dialogueText.text += letter;
+                letters += 1;
+                //TODO: add sound effects for typing character by character
+                //TODO: add a thing that makes it so a word jumps to the next line if it can't fit completely on the current line
+                //TODO: add a thing 
+                yield return new WaitForSeconds(typingSpeed);
+            }
+        }
+        // display choices if any
+        DisplayChoices();
+        canContinueIcon.SetActive(true);
+        canContinueToNextLine = true;
+    }
     private void DisplayChoices()
     {
         List<Choice> currentChoices = currentStory.currentChoices;
@@ -137,15 +201,26 @@ public class DialogueManager : MonoBehaviour
             choices[i].gameObject.SetActive(false);
         }
     }
+    private void HideChoices()
+    {
+        foreach (GameObject c in choices)
+        {
+            c.SetActive(false);
+        }
+    }
 
     public void MakeChoice(int i)
     {
-        currentStory.ChooseChoiceIndex(i);
-        ContinueStory();
+        if (canContinueToNextLine)
+        {
+            currentStory.ChooseChoiceIndex(i);
+            ContinueStory();
+        }
     }
 
     private void HandleTags(List<String> tags)
     {
+        //resetTags();
         foreach (string tag in tags)
         {
             //make a list w/ the tag and tag value
@@ -163,17 +238,29 @@ public class DialogueManager : MonoBehaviour
                 case SPEAKER_TAG:
                     displayNameText.text = tagValue;
                     break;
+
                 case PORTRAIT_TAG:
-                    portraitAnimator.Play(tagValue);
-                    // if the previous Play() call failed it won't be playing, so play the default portrait
-                    if (!isPlaying(portraitAnimator, tagValue))
+                    bool isValidPortraitName = false;
+                    for (int i = 0; i < portraitClips.Length; i++) 
+                    {
+                        if (portraitClips[i].name.Equals(tagValue))
+                        {
+                            isValidPortraitName = true;
+                        }
+                    }
+                    if (isValidPortraitName)
+                    {
+                        portraitAnimator.Play(tagValue);
+                    }
+                    else
                     {
                         portraitAnimator.Play("default");
+                        Debug.LogWarning("Portrait Tag Value: " + tagValue + " is not in the list of animation clips, playing default (maybe it's named wrong in INK/portrait animation clip)");
                     }
                     break;
+
                 case LAYOUT_TAG:
                     layoutAnimator.Play(tagValue);
-                    Debug.Log(tagKey + tagValue);
                     break;
                 default:
                     Debug.LogWarning("Tag isn't one of the defined keys: " + tag);
